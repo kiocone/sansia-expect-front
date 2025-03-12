05f062b6793105b6d79f0f29c97152e91797af8f
kiocone <kiocone@gmail.com>
2025-03-12 10:28:35 -0500

    feat: add tayrona images

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -84,7 +84,14 @@ export class PostServices {
         "../../assets/images/tay-04.jpeg",
         "../../assets/images/tay-05.jpeg",
         "../../assets/images/tay-06.jpeg",
-        "../../assets/images/tay-07.jpeg"
+        "../../assets/images/tay-07.jpeg",
+        "../../assets/images/tay-08.jpeg",
+        "../../assets/images/tay-09.jpeg",
+        "../../assets/images/tay-10.jpeg",
+        "../../assets/images/tay-11.jpeg",
+        "../../assets/images/tay-12.jpeg",
+        "../../assets/images/tay-13.jpeg",
+        "../../assets/images/tay-14.jpeg"   
       ]
     }
   ]
